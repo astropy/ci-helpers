7617083cb5fc067ac5c1cce9051341518ed12f38
Brigitta Sipocz <bsipocz@gmail.com>
2015-11-10 17:20:30 +0000

    pytest install is always needed

--- appveyor/install-miniconda.ps1
+++ appveyor/install-miniconda.ps1
@@ -86,4 +86,4 @@ activate test
 python --version
 
 # Install the specified versions of numpy as astropy and other dependencies
-conda install numpy=%NUMPY_VERSION% astropy=%ASTROPY_VERSION% %CONDA_ADDITIONAL_DEPENDENCIES%
+conda install pytest numpy=%NUMPY_VERSION% astropy=%ASTROPY_VERSION% %CONDA_ADDITIONAL_DEPENDENCIES%
