484d7e2d9e3b2e0687503b4a4cb9cd94dd3f857f
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-11-11 08:12:22 +0100

    Renamed CONDA_ADDITIONAL_DEPENDENCIES to CONDA_DEPENDENCIES

--- appveyor/install-miniconda.ps1
+++ appveyor/install-miniconda.ps1
@@ -86,4 +86,4 @@ activate test
 python --version
 
 # Install the specified versions of numpy as astropy and other dependencies
-conda install pytest numpy=$env:NUMPY_VERSION astropy=$env:ASTROPY_VERSION $env:CONDA_ADDITIONAL_DEPENDENCIES.Split(" ")
+conda install pytest numpy=$env:NUMPY_VERSION astropy=$env:ASTROPY_VERSION $env:CONDA_DEPENDENCIES.Split(" ")
