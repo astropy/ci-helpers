479d03eba26e7b53fd32080de73ea35d18235a97
Brigitta Sipocz <bsipocz@gmail.com>
2015-11-29 18:18:54 +0000

    Adding extra echos

--- travis/setup_conda_linux.sh
+++ travis/setup_conda_linux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "==================== Starting executing ci-helpers scripts ====================="
+
 # Install conda
 # http://conda.pydata.org/docs/travis.html#the-travis-yml-file
 wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
@@ -8,3 +10,5 @@ export PATH="$HOME/miniconda/bin:$PATH"
 
 # Install common Python dependencies
 source "$( dirname "${BASH_SOURCE[0]}" )"/setup_dependencies_common.sh
+
+echo "================= Returning executing local .travis.yml script ================="
--- travis/setup_conda_osx.sh
+++ travis/setup_conda_osx.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "==================== Starting executing ci-helpers scripts ====================="
+
 # Install conda
 # http://conda.pydata.org/docs/travis.html#the-travis-yml-file
 wget http://repo.continuum.io/miniconda/Miniconda-3.7.3-MacOSX-x86_64.sh -O miniconda.sh
@@ -8,3 +10,5 @@ export PATH="$HOME/miniconda/bin:$PATH"
 
 # Install common Python dependencies
 source "$( dirname "${BASH_SOURCE[0]}" )"/setup_dependencies_common.sh
+
+echo "================= Returning executing local .travis.yml script ================="
