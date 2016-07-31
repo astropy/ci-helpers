89ba578dc8e7bd44c8620b7e93d2d388a98b2130
David PS <dps.helio@gmail.com>
2016-07-31 15:26:30 +0100

    Added miniconda 2 or 3 option
    This is to avoid donwloading py2 stuff and then upgrade

--- travis/setup_conda_linux.sh
+++ travis/setup_conda_linux.sh
@@ -8,7 +8,12 @@ echo "==================== Starting executing ci-helpers scripts ===============
 
 # Install conda
 # http://conda.pydata.org/docs/travis.html#the-travis-yml-file
-wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
+if [[ "$PYTHON_VERSION" == "2.7" ]]; then
+      wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
+else
+      wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+fi
+
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 
