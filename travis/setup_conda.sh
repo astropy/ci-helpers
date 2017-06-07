e9d60301f961203830cc65f5fb51cbc3b86ff8ea
Brigitta Sipocz <bsipocz@gmail.com>
2017-06-07 16:28:05 +0100

    Moving the custom tag checking before anything is installed. Testing it by: [skip travis] [skip appveyor]

--- travis/setup_conda.sh
+++ travis/setup_conda.sh
@@ -7,10 +7,23 @@
 #
 # The present script was added later.
 
+
+# We first check if any of the custom tags are used to skip the build
+
+TR_SKIP="\[(skip travis|travis skip)\]"
+DOCS_ONLY="\[docs only|build docs\]"
+
 # Skip build if the commit message contains [skip travis] or [travis skip]
 # Remove workaround once travis has this feature natively
 # https://github.com/travis-ci/travis-ci/issues/5032
-echo "$TRAVIS_COMMIT_MESSAGE" | grep -E  '\[(skip travis|travis skip)\]' \
-    && echo "[skip travis] has been found, exiting." && exit 0
+if [[ ! -z $(echo $TRAVIS_COMMIT_MESSAGE | grep -E $TR_SKIP) ]]; then
+    echo "Travis was requested to be skipped by the commit message, exiting."
+    travis_terminate 0
+elif [[ ! -z $(echo $TRAVIS_COMMIT_MESSAGE | grep -E $DOCS_ONLY) ]]; then
+    if [[ $SETUP_CMD != *build_docs* ]] || [[ $SETUP_CMD != *build_sphinx* ]]; then
+        echo "Only docs build was requested by the commit message, exiting."
+        travis_terminate 0
+    fi
+fi
 
 source ci-helpers/travis/setup_conda_$TRAVIS_OS_NAME.sh;
