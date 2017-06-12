9e391f276820c561d11d47fadec93ad804de3226
Pes2009j <Pes2009j@users.noreply.github.com>
2017-06-13 03:25:31 +0430

    Revert "Numpy stable version updated to 1.13"

--- travis/setup_conda_osx.sh
+++ travis/setup_conda_osx.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# Skip build if the commit message contains [skip travis] or [travis skip]
+# Remove workaround once travis has this feature natively
+# https://github.com/travis-ci/travis-ci/issues/5032
+echo "$TRAVIS_COMMIT_MESSAGE" | grep -E  '\[(skip travis|travis skip)\]' \
+    && echo "[skip travis] has been found, exiting." && exit 0
+
+if [[ $DEBUG == True ]]; then
+    set -x
+fi
+
 # Workaround for https://github.com/travis-ci/travis-ci/issues/6307, which
 # caused the following error on MacOS X workers:
 #
 # /Users/travis/build.sh: line 109: shell_session_update: command not found
 #
 rvm get head
+
+echo "==================== Starting executing ci-helpers scripts ====================="
 
 # Install conda
 # http://conda.pydata.org/docs/travis.html#the-travis-yml-file
@@ -15,3 +27,5 @@ export PATH="$HOME/miniconda/bin:$PATH"
 
 # Install common Python dependencies
 source "$( dirname "${BASH_SOURCE[0]}" )"/setup_dependencies_common.sh
+
+echo "================= Returning executing local .travis.yml script ================="
