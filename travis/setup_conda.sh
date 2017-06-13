d5ab8d14926ded2f9471e3ae0df883164430902f
Brigitta Sipocz <bsipocz@gmail.com>
2017-06-13 14:34:39 +0100

    Make sure we switch off debug mode

--- travis/setup_conda.sh
+++ travis/setup_conda.sh
@@ -47,3 +47,5 @@ echo "==================== Starting executing ci-helpers scripts ===============
 source ci-helpers/travis/setup_conda_$TRAVIS_OS_NAME.sh;
 
 echo "================= Returning executing local .travis.yml script ================="
+
+set +x
