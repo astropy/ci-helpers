ca49b54ef3aa40c8f2432e1c2d78ba2824ea3796
Brigitta Sipocz <bsipocz@gmail.com>
2017-07-07 18:29:56 +0100

    Updating latest and lts astropy to 2.0

--- appveyor/install-miniconda.ps1
+++ appveyor/install-miniconda.ps1
@@ -5,8 +5,8 @@
 
 $MINICONDA_URL = "https://repo.continuum.io/miniconda/"
 
-$env:ASTROPY_LTS_VERSION = "1.0"
-$env:LATEST_ASTROPY_STABLE = "1.3"
+$env:ASTROPY_LTS_VERSION = "2.0"
+$env:LATEST_ASTROPY_STABLE = "2.0"
 $env:LATEST_NUMPY_STABLE = "1.13"
 
 # We pin the version for conda as it's not the most stable package from
