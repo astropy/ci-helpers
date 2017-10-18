508936fd6d0afb90c244ec2b12f0a0e0bf663c43
Nabobalis <nabil.freij@gmail.com>
2017-10-18 11:36:29 -0400

    update to new sunpy v0.8.2 for stable

--- appveyor/install-miniconda.ps1
+++ appveyor/install-miniconda.ps1
@@ -34,7 +34,7 @@ $MINICONDA_URL = "https://repo.continuum.io/miniconda/"
 $env:ASTROPY_LTS_VERSION = "2.0.2"
 $env:LATEST_ASTROPY_STABLE = "2.0.2"
 $env:LATEST_NUMPY_STABLE = "1.13"
-$env:LATEST_SUNPY_STABLE = "0.8.1"
+$env:LATEST_SUNPY_STABLE = "0.8.2"
 
 # We pin the version for conda as it's not the most stable package from
 # release to release. Add note here if version is pinned due to a bug upstream.
