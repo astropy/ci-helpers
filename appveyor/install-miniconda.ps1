0e28f95999d4235c3ebd60e7b08cdcb36b7905e5
Brigitta Sipocz <bsipocz@gmail.com>
2018-08-03 16:22:07 +0100

    Updating stable numpy to 1.15

--- appveyor/install-miniconda.ps1
+++ appveyor/install-miniconda.ps1
@@ -41,7 +41,7 @@ else {
 }
 
 $env:ASTROPY_LTS_VERSION = "2.0.7"
-$env:LATEST_NUMPY_STABLE = "1.14"
+$env:LATEST_NUMPY_STABLE = "1.15"
 $env:LATEST_SUNPY_STABLE = "0.9.2"
 
 # We pin the version for conda as it's not the most stable package from
