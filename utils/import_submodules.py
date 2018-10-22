c9046c13ebe6f5d00f62a28feb6009c1f1851a6c
Brigitta Sipocz <bsipocz@gmail.com>
2018-10-22 15:48:45 -0700

    Adding utility function to test importing all submodules from packages

---
+++ utils/import_submodules.py
@@ -0,0 +1,39 @@
+import sys
+import pkgutil
+import importlib
+
+
+def import_submodules(package, recursive=True):
+    """
+    Import all submodules of a module, recursively, including subpackages
+    Original form is from StackOverflow.
+
+    Parameters
+    ----------
+
+    package : str or module name
+        name of the package or the imported package to import the
+        submodules from.
+    recursive : bool
+        Import only the top level if recursive is False.
+    """
+    if isinstance(package, str):
+        package = importlib.import_module(package)
+    results = {}
+    for loader, name, is_pkg in pkgutil.walk_packages(package.__path__):
+        full_name = package.__name__ + '.' + name
+        try:
+            results[full_name] = importlib.import_module(full_name)
+            print(full_name)
+        except ImportError:
+            if name.startswith('_'):
+                continue
+        if recursive and is_pkg:
+            results.update(import_submodules(full_name))
+    return results
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        raise IndexError("specify a packagename to import.")
+    import_submodules(sys.argv[1])
