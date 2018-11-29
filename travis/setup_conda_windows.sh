e2d328a2b410a52566da19de923b6d3a732c82e1
Daniel D'Avella <drdavella@gmail.com>
2018-11-29 12:55:11 -0500

    Add debugging information to windows script

--- travis/setup_conda_windows.sh
+++ travis/setup_conda_windows.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # This script assumes we are running under git-bash (MinGW) on Windows
 
+if [[ $DEBUG == True ]]; then
+    set -x
+fi
+
 if [[ -z "${MINICONDA_VERSION}" ]]; then
     MINICONDA_VERSION=4.5.4
 fi
@@ -13,4 +17,5 @@ PIN_FILE_CONDA="/c/tools/miniconda3/conda-meta/pinned"
 PIN_FILE="/c/tools/miniconda3/envs/test/conda-meta/pinned"
 
 # Install common Python dependencies
+echo "setting up common dependencies"
 source "$( dirname "${BASH_SOURCE[0]}" )"/setup_dependencies_common.sh
