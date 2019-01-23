7dd469c52525d25405ecfaa91f385416c9b253e6
zeman <zeman@icp.uni-stuttgart.de>
2019-01-24 00:19:43 +0100

    overridable env variable MACOSX_DEPLOYMENT_TARGET=10.9

--- travis/setup_conda_osx.sh
+++ travis/setup_conda_osx.sh
@@ -15,6 +15,16 @@ rvm get stable
 if [[ -z "${MINICONDA_VERSION}" ]]; then
     MINICONDA_VERSION=4.5.12
 fi
+
+# Set default OSX deployment target version to 10.9, since this is required for
+# compiling C++ code with llvm/clang when -stdlib=libc++ is specified.
+# Note that, for linking, version 10.7 should suffice.
+if [[ -z "${MACOSX_DEPLOYMENT_TARGET}" ]]; then
+    export MACOSX_DEPLOYMENT_TARGET=10.9
+elif [[ "${MACOSX_DEPLOYMENT_TARGET}" == "clang_default" ]]; then
+    export MACOSX_DEPLOYMENT_TARGET=""
+fi
+
 wget https://repo.continuum.io/miniconda/Miniconda3-${MINICONDA_VERSION}-MacOSX-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
