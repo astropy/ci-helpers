23ad6efe3a92dcf08a6b93efdce4492a3d5236a0
Matthew Craig <mattwcraig@gmail.com>
2019-08-02 22:27:28 -0400

    Use 4.6 on windows because that is what chocolatey has

--- travis/setup_conda_windows.sh
+++ travis/setup_conda_windows.sh
@@ -6,12 +6,13 @@ if [[ $DEBUG == True ]]; then
 fi
 
 if [[ -z "${MINICONDA_VERSION}" ]]; then
-    MINICONDA_VERSION=4.7.10
+    MINICONDA_VERSION=4.6.14
 fi
 
 echo "installing miniconda3"
 choco install miniconda3 --params="'/AddToPath:1'" --version="$MINICONDA_VERSION";
-export PATH="/c/tools/miniconda3/scripts:/c/tools/miniconda3/:$PATH";
+conda init bash
+conda activate base
 
 PIN_FILE_CONDA="/c/tools/miniconda3/conda-meta/pinned"
 PIN_FILE="/c/tools/miniconda3/envs/test/conda-meta/pinned"
