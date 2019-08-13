bea49496762094c29424d65e62f158c3327689f6
Matt Craig <mattwcraig@gmail.com>
2019-08-13 16:41:57 -0500

    Modernize conda activation in Windows
    Co-Authored-By: David Hoese <david.hoese@ssec.wisc.edu>

--- travis/setup_conda_windows.sh
+++ travis/setup_conda_windows.sh
@@ -11,8 +11,9 @@ fi
 
 echo "installing miniconda3"
 choco install miniconda3 --params="'/AddToPath:1'" --version="$MINICONDA_VERSION";
-export PATH="/c/tools/miniconda3/scripts:/c/tools/miniconda3/bin:$PATH"
+export PATH="/c/tools/miniconda3/scripts:$PATH"
 conda init bash
+source /c/Users/travis/.bash_profile
 conda activate base
 
 PIN_FILE_CONDA="/c/tools/miniconda3/conda-meta/pinned"
