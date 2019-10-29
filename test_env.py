714d449550d4f626e1ba4ea36362e8232ef4529a
Brigitta Sipocz <bsipocz@gmail.com>
2019-10-28 21:00:31 -0700

    Fix appveyor testing

--- test_env.py
+++ test_env.py
@@ -34,15 +34,18 @@ if 'TRAVIS_REPO_SLUG' in os.environ:
 
 if not LooseVersion(sys.version) < '3.5':
     LATEST_ASTROPY_STABLE = '3.2.3'
-    LATEST_ASTROPY_STABLE_WIN = '3.2.3'
+    # This is not for windows but appveyor only
+    LATEST_ASTROPY_STABLE_WIN = '3.2.2'
     LATEST_NUMPY_STABLE = '1.17'
 else:
     LATEST_ASTROPY_STABLE = '2.0.16'
-    LATEST_ASTROPY_STABLE_WIN = '2.0.16'
+    # This is not for windows but appveyor only
+    LATEST_ASTROPY_STABLE_WIN = '2.0.15'
     LATEST_NUMPY_STABLE = '1.16'
 
 LATEST_ASTROPY_LTS = '2.0.16'
-LATEST_ASTROPY_LTS_WIN = '2.0.16'
+# This is not for windows but appveyor only
+LATEST_ASTROPY_LTS_WIN = '2.0.15'
 LATEST_NUMPY_STABLE_WIN = '1.16'
 LATEST_SUNPY_STABLE = '1.0.3'
 
