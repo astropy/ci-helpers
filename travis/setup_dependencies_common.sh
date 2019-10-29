6e831dfc3a891cb42867a4ef7fbf0e17ec52fee8
Brigitta Sipocz <bsipocz@gmail.com>
2019-10-28 17:55:47 -0700

    Updating versions

--- travis/setup_dependencies_common.sh
+++ travis/setup_dependencies_common.sh
@@ -150,13 +150,14 @@ fi
 # We will use the 2.0.x releases as "stable" for Python 2.7 and 3.4
 if [[ $(python -c "from distutils.version import LooseVersion; import os;\
         print(LooseVersion(os.environ['PYTHON_VERSION']) < '3.5')") == False ]]; then
-    export LATEST_ASTROPY_STABLE=3.2.2
+    export LATEST_ASTROPY_STABLE=3.2.3
+    export LATEST_NUMPY_STABLE=1.17
 else
-    export LATEST_ASTROPY_STABLE=2.0.15
+    export LATEST_ASTROPY_STABLE=2.0.16
     export NO_PYTEST_ASTROPY=True
+    export LATEST_NUMPY_STABLE=1.16
 fi
-export ASTROPY_LTS_VERSION=2.0.15
-export LATEST_NUMPY_STABLE=1.17
+export ASTROPY_LTS_VERSION=2.0.16
 export LATEST_SUNPY_STABLE=1.0.3
 
 
