edfd39315121c336ea5c23c58ac2faad8f0a4f33
David Hoese <david.hoese@ssec.wisc.edu>
2019-11-17 19:49:30 -0600

    Remove usage of tee to avoid read-only issues on Windows

--- travis/setup_dependencies_common.sh
+++ travis/setup_dependencies_common.sh
@@ -90,8 +90,11 @@ function retry_on_known_error() {
             revised_command=$(cat $_tmp_updated_conda_command)
             echo $revised_command
             # Try it; if it still has conflicts then just give up
-            $revised_command > >(tee $_tmp_output_file) 2>&1
+            $revised_command > $_tmp_output_file 2>&1
             _exitval="$?"
+            # Keep the cat here...otherwise _exitval is always 0
+            # even if the conda install failed.
+            cat $_tmp_output_file
             if [[ -n $(grep "conflicts with explicit specs" $_tmp_output_file) ]]; then
                 echo "STOPPING conda attempts because unable to resolve conda pinning issues"
                 rm -f $_tmp_output_file
