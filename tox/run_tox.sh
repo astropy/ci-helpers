155ffdbbb52dfc4d960ffd07045fa7fced06e011
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-03-17 22:51:13 +0000

    Allow python interpreter to be overriden and make sure pip and setuptools are recent

--- tox/run_tox.sh
+++ tox/run_tox.sh
@@ -3,6 +3,7 @@
 # This script installs and runs tox, assuming that the following environment
 # variables are defined:
 #
+# * PYTHON: the Python interpreter to use (defaults to 'python')
 # * TOXENV: the name of the tox environment to run
 # * TOXARGS: the arguments to pass to tox
 # * TOXPOSARGS: the positional arguments to pass after the -- separator
@@ -18,10 +19,15 @@
 
 set -e
 
+if [ -z "$PYTHON" ]; then
+  PYTHON=python;
+fi
+
 echo '########################################################################'
 echo ''
 echo 'ci-helpers run_tox.sh script'
 echo ''
+echo 'PYTHON='$PYTHON
 echo 'TOXENV='$TOXENV
 echo 'TOXARGS='$TOXARGS
 echo 'TOXPOSARGS='$TOXPOSARGS
@@ -38,17 +44,21 @@ echo 'Installing tox:'
 echo ''
 
 # Start off by installing tox
-pip install tox
+$PYTHON -m pip install tox
+
+# Ensure pip and setuptools are recent (we don't force upgrade to the
+# absolute most recent version since this is not necessary)
+$PYTHON -m pip install "pip>=19.3.1" "setuptools>=30.3.0"
 
 # If PyPI patches are needed, uncommend the following line
-pip install tox-pypi-filter
+$PYTHON -m pip install tox-pypi-filter
 
 echo ''
 echo 'Listing Python packages:'
 echo ''
 
 # List the current installed packages
-pip freeze
+$PYTHON -m pip freeze
 
 echo ''
 echo 'Running tox:'
@@ -56,6 +66,6 @@ echo ''
 
 # Run tox. If PyPI patches are needed, add a --pypi-filter=...
 # option to the command, e.g. --pypi-filter='pytest<5'
-tox -e $TOXENV $TOXARGS --pypi-filter='pytest<5.4' -- $TOXPOSARGS
+$PYTHON -m tox -e $TOXENV $TOXARGS --pypi-filter='pytest<5.4' -- $TOXPOSARGS
 echo ''
 echo '########################################################################'
