56603b68edaacb335aba0c74478b45b20f5b1995
ossdev07 <ossdev@puresoftware.com>
2020-04-22 10:47:45 +0530

    Replaced Archiconda With Miniforge
    Signed-off-by: ossdev07 <ossdev@puresoftware.com>

--- travis/setup_conda_linux.sh
+++ travis/setup_conda_linux.sh
@@ -1,41 +1,27 @@
 #!/bin/bash
-
 # Install conda (http://conda.pydata.org/docs/travis.html#the-travis-yml-file)
 # Note that we pin the Miniconda version to avoid issues when new versions are released.
 # This can be updated from time to time.
 if [[ -z "${MINICONDA_VERSION}" ]]; then
     MINICONDA_VERSION=4.7.10
 fi
-
 if [ `uname -m` = 'aarch64' ]; then
-   sudo apt-get install python3 python3-dev python3-setuptools cython cython3 python3-pip gcc gfortran libblas-dev liblapack-dev;
-   wget -q "https://github.com/Archiconda/build-tools/releases/download/0.2.3/Archiconda3-0.2.3-Linux-aarch64.sh" -O archiconda.sh
-   chmod +x archiconda.sh
-   bash archiconda.sh -b -p $HOME/miniconda
-   export PATH="$HOME/miniconda/bin:$PATH"
-   sudo cp -r $HOME/miniconda/bin/* /usr/bin/
-   hash -r
-   sudo conda config --set always_yes yes --set changeps1 no
-   sudo conda update -q conda
-   sudo conda info -a
-   source activate base
-   source "$( dirname "${BASH_SOURCE[0]}" )"/setup_dependencies_common.sh
+   wget -q "https://github.com/conda-forge/miniforge/releases/download/4.8.2-1/Miniforge3-4.8.2-1-Linux-aarch64.sh" -O miniconda.sh
+   chmod +x miniconda.sh
 else
-   if [[ -z "${MINICONDA_VERSION}" ]]; then
-    MINICONDA_VERSION=4.7.10
-   fi
    wget https://repo.continuum.io/miniconda/Miniconda3-${MINICONDA_VERSION}-Linux-x86_64.sh -O miniconda.sh --progress=dot:mega
    # Create .conda directory before install to workaround conda bug
    # See https://github.com/ContinuumIO/anaconda-issues/issues/11148
-   mkdir $HOME/.conda
-   bash miniconda.sh -b -p $HOME/miniconda
-   $HOME/miniconda/bin/conda init bash
-   source ~/.bash_profile
-   conda activate base
-   source "$( dirname "${BASH_SOURCE[0]}" )"/setup_dependencies_common.sh
-   if [[ $SETUP_XVFB == True ]]; then
+fi
+mkdir $HOME/.conda
+bash miniconda.sh -b -p $HOME/miniconda
+$HOME/miniconda/bin/conda init bash
+source ~/.bash_profile
+export PATH=$HOME/miniconda/bin/:$PATH
+source activate base
+# Install common Python dependencies
+source "$( dirname "${BASH_SOURCE[0]}" )"/setup_dependencies_common.sh
+if [[ $SETUP_XVFB == True ]]; then
     export DISPLAY=:99.0
     /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -screen 0 1920x1200x24 -ac +extension GLX +render -noreset
-   fi
 fi
-export PATH=$MINICONDA_DIR/bin:$PATH
--- travis/setup_dependencies_common.sh
+++ travis/setup_dependencies_common.sh
@@ -1,12 +1,9 @@
 #!/bin/bash -x
+
 hash -r
 
 set -e
-if [ `uname -m` = 'aarch64' ]; then
-    sudo chmod -R 777 /home/travis/miniconda
-    sudo chmod -R 777 /home/travis/.condarc
-    sudo rm /usr/bin/lsb_release
-fi
+
 
 # If not set from outside, initialize parameters for the retry_on_known_error()
 # function:
@@ -159,15 +156,9 @@ if [[ $(python -c "from distutils.version import LooseVersion; import os;\
     export LATEST_ASTROPY_STABLE=4.0
     export LATEST_NUMPY_STABLE=1.18
 else
-     if [[ $(python -c "from distutils.version import LooseVersion; import os;\
-            print(LooseVersion(os.environ['PYTHON_VERSION']) < '3.5')") == False ]]; then
-        export LATEST_ASTROPY_STABLE=4.0
-        export LATEST_NUMPY_STABLE=1.18
-     else
-        export LATEST_ASTROPY_STABLE=2.0.16
-        export NO_PYTEST_ASTROPY=True
-        export LATEST_NUMPY_STABLE=1.16
-     fi
+    export LATEST_ASTROPY_STABLE=2.0.16
+    export NO_PYTEST_ASTROPY=True
+    export LATEST_NUMPY_STABLE=1.16
 fi
 export ASTROPY_LTS_VERSION=4.0
 export LATEST_SUNPY_STABLE=1.0.6
@@ -200,28 +191,31 @@ fi
 # release to release. Add note here if version is pinned due to a bug upstream.
 if [[ -z $CONDA_VERSION ]]; then
     if [ `uname -m` = 'aarch64' ]; then
-        CONDA_VERSION=4.5.12
+        CONDA_VERSION=4.8.2
     else
-	CONDA_VERSION=4.7.11
+        CONDA_VERSION=4.7.11
     fi
 fi
+
 if [[ -z $PIN_FILE_CONDA ]]; then
     PIN_FILE_CONDA=$HOME/miniconda/conda-meta/pinned
 fi
-echo "conda ${CONDA_VERSION}" > $PIN_FILE_CONDA
-retry_on_known_error conda install $QUIET conda
-if [ `uname -m` != 'aarch64' ]; then
-    if [[ -z $CONDA_CHANNEL_PRIORITY ]]; then
-        CONDA_CHANNEL_PRIORITY=disabled
-    else
-        # Make lowercase
-        CONDA_CHANNEL_PRIORITY=$(echo $CONDA_CHANNEL_PRIORITY | awk '{print tolower($0)}')
-    fi
 
-	# We need to add this after the update, otherwise the ``channel_priority``
-	# key may not yet exists
-    conda config  --set channel_priority $CONDA_CHANNEL_PRIORITY
+echo "conda ${CONDA_VERSION}" > $PIN_FILE_CONDA
+
+retry_on_known_error conda install $QUIET conda
+
+if [[ -z $CONDA_CHANNEL_PRIORITY ]]; then
+    CONDA_CHANNEL_PRIORITY=disabled
+else
+    # Make lowercase
+    CONDA_CHANNEL_PRIORITY=$(echo $CONDA_CHANNEL_PRIORITY | awk '{print tolower($0)}')
 fi
+
+# We need to add this after the update, otherwise the ``channel_priority``
+# key may not yet exists
+conda config  --set channel_priority $CONDA_CHANNEL_PRIORITY
+
 # Use utf8 encoding. Should be default, but this is insurance against
 # future changes
 export PYTHONIOENCODING=UTF8
@@ -232,7 +226,6 @@ if [[ ! -z $PYTHON_VERSION ]]; then
 else
     PYTHON_OPTION=""
 fi
-
 
 # Setting the MPL backend to a default to avoid occational segfaults with the qt backend
 if [[ -z $MPLBACKEND ]]; then
@@ -248,22 +241,12 @@ fi
 
 
 # CONDA
-if [ `uname -m` == 'aarch64' ]; then
-   if [[ -z $CONDA_ENVIRONMENT ]]; then
-    retry_on_known_error conda create -q -n test  python=3.7
-   else
-    retry_on_known_error conda create -q -n test python=3.7 $CONDA_ENVIRONMENT
-   fi
-   source activate test
-else
-   if [[ -z $CONDA_ENVIRONMENT ]]; then
+if [[ -z $CONDA_ENVIRONMENT ]]; then
     retry_on_known_error conda create $QUIET -n test $PYTHON_OPTION
-   else
+else
     retry_on_known_error conda env create $QUIET -n test -f $CONDA_ENVIRONMENT
-   fi
-   conda activate test
 fi
-
+conda activate test
 
 # PIN FILE
 if [[ -z $PIN_FILE ]]; then
@@ -366,11 +349,9 @@ fi
 # Pin required versions for dependencies, howto is in FAQ of conda
 # https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-pkgs.html#preventing-packages-from-updating-pinning
 if [[ ! -z $CONDA_DEPENDENCIES ]]; then
-    if [ `uname -m` ! = 'aarch64' ]; then
         if [[ -z $(echo $CONDA_DEPENDENCIES | grep '\bmkl\b') &&
-                 $TRAVIS_OS_NAME != windows && ! -z $NUMPY_VERSION ]]; then
-            CONDA_DEPENDENCIES=${CONDA_DEPENDENCIES}" nomkl"
-        fi
+                    $TRAVIS_OS_NAME != windows && ! -z $NUMPY_VERSION ]]; then
+                CONDA_DEPENDENCIES=${CONDA_DEPENDENCIES}" nomkl"
     fi
 
     # The astropy testrunner is not compatible with coverage 5.0+, thus we limit the version
@@ -498,7 +479,6 @@ if [[ ! -z $NUMPY_VERSION ]]; then
 fi
 
 # We use --no-pin to avoid installing other dependencies just yet.
-
 
 if [ `uname -m` = aarch64]; then
     MKL='';
